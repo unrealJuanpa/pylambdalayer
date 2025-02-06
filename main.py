9dce8b50b41490c58d4ebfccbed420442d772df5
unrealJuanpa <juanpagramajo@icloud.com>
2025-02-06 00:11:31 -0600

    Fixed: usage of python3

--- main.py
+++ main.py
@@ -13,17 +13,9 @@ def clear_directory(path):
         shutil.rmtree(path)
     os.makedirs(path)
 
-def install_packages(packages, install_path, architecture):
-    if architecture == 'arm64':
-        for package in packages:
-            subprocess.check_call([
-                'docker', 'run', '--rm', '--platform', 'linux/arm64', 
-                '-v', f"{os.getcwd()}:/app", '-w', '/app', 'python:3.9', 
-                'bash', '-c', f"pip install {package} -t {install_path}"
-            ])
-    else:
-        for package in packages:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package, '-t', install_path, '--only-binary=:all:'])
+def install_packages(packages, install_path):
+    for package in packages:
+        subprocess.check_call([sys.executable, '-m', 'pip3', 'install', package, '-t', install_path, '--only-binary=:all:'])
 
 def create_zip(source_path, zip_name):
     with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
@@ -34,7 +26,6 @@ def create_zip(source_path, zip_name):
 
 def main():
     zip_name = input("Enter the name of the zip file (don't include .zip in the name): ")
-    architecture = input("Enter the architecture (x86_64 or arm64): ")
     packages = []
     while True:
         package = input("Enter a package (or press Enter to finish): ")
@@ -49,7 +40,7 @@ def main():
     clear_directory('files')
     create_directory(base_path)
     create_directory(zip_path)
-    install_packages(packages, base_path, architecture)
+    install_packages(packages, base_path)
     create_zip('files/python', os.path.join(zip_path, f'{zip_name}.zip'))
 
     print(f"'{zip_name}.zip' file created successfully.")
